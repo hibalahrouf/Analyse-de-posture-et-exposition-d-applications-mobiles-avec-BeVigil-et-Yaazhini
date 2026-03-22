c0238f396bd1991026fe153f2d364e2ca6471723
hibalahrouf <yaraema78@gmail.com>
2026-03-22 22:23:56 +0100

    Delete make_readme.py

--- make_readme.py
+++
@@ -1,7 +0,0 @@
-﻿import os
-os.chdir(r'C:\Users\pc\OneDrive\Desktop\lab-mobile-security')
-
-readme = open('README.md', 'w', encoding='utf-8')
-readme.write('debut')
-readme.close()
-print('README.md cree !')
